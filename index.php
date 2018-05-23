baf885b6801e2f5f4805d14334f8aebe3481efca
eldeante <39552194+eldeante@users.noreply.github.com>
2018-05-23 12:22:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -895,7 +895,7 @@ else
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#"><strong>Stock</strong>PETRONICS</a>
+          <a class="navbar-brand" href="#"><strong>Stock</strong>EJEMPLO</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">                 
